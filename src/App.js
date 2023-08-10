3b4e980141d5058eb3faa0e0a65d1b8bb661b2b8
JACKS9969 <101617802+JACKS9969@users.noreply.github.com>
2023-08-10 12:15:56 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import { useState } from 'react'
+
 import { Link } from 'react-router-dom'
 import './App.css'
 
 import CustomRoutes from './routes/CustomRoutes'
-import Pokedex from './components/Pokedex/Pokedex'
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <div>
       <Link className='pokedex' to='/'>Pokedex</Link>
       <CustomRoutes />
-      <Pokedex />
-      
     </div>
   )
 }
